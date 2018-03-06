40dcb798f0d5d2ac3518987e803af06417911f71
remicck <remicck@gmail.com>
2018-03-06 19:13:35 +0900

    fixed: 'anchor-is-valid' rule interfering with react-router-dom

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,11 @@ module.exports = {
     "no-underscore-dangle": [
       "error",
       { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }
-    ]
+    ],
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['to'],
+      aspects: ['noHref', 'invalidHref', 'preferButton'],
+    }],
   }
 };
