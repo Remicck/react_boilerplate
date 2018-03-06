67be14860ebd0dc107036df494484b26b8a77c82
remicck <remicck@gmail.com>
2018-03-06 19:39:34 +0900

    modified: change link color, black to white

--- src/components/linklist.jsx
+++ src/components/linklist.jsx
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom';
 // material-ui
 import FlatButton from 'material-ui/FlatButton';
 
+const style = {
+  color: 'white',
+};
+
 /*
   これはfunctional Componentと言って、state管理の必要が無い場合に使うコンポーネントです。
 */
 function LinkList() {
   return (
     <div>
-      <FlatButton label="Top" containerElement={<Link to="/" />} />
-      <FlatButton label="タイトル1" containerElement={<Link to="/title1" />} />
-      <FlatButton label="タイトル2" containerElement={<Link to="/title2" />} />
+      <FlatButton label="Top" labelStyle={style}containerElement={<Link to="/" />} />
+      <FlatButton label="タイトル1" labelStyle={style}containerElement={<Link to="/title1" />} />
+      <FlatButton label="タイトル2" labelStyle={style}containerElement={<Link to="/title2" />} />
     </div>
   );
 }
